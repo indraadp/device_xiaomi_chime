b834bd4b5dfe72af4aa2130ffb804951db1b7560
frost. (frostg-012) <frost@nusantararom.org>
2022-09-03 12:39:42 +0700

    NusantaraProject bringup!!
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: Ibb2072b06c7b5570f054dc90117d8a5e8496b8d2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_chime.mk
+    $(LOCAL_DIR)/nad_chime.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_chime-user \
-    aosp_chime-userdebug \
-    aosp_chime-eng
+    nad_chime-user \
+    nad_chime-userdebug \
+    nad_chime-eng
--- nad_chime.mk
+++ nad_chime.mk
@@ -11,16 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from chime device
 $(call inherit-product, device/xiaomi/chime/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+# Inherit some common NusantaraProject stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+TARGET_USES_BLUR := true
+USE_GAPPS := true
+USE_PIXEL_CHARGING := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+NAD_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_chime
+PRODUCT_NAME := nad_chime
 PRODUCT_DEVICE := chime
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 9T
+PRODUCT_MODEL := SM6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

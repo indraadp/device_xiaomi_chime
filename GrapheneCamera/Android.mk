a9f2ece26cdef253cf445e10f0466c85376325e2
indraadp <indraproject777@gmail.com>
2023-06-04 13:24:59 +0700

    chime: update Graphene Camera v62
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- GrapheneCamera/Android.mk
+++ GrapheneCamera/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := GrapheneCamera
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := GrapheneCamera.apk
+LOCAL_SRC_FILES := GrapheneCamera62.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
